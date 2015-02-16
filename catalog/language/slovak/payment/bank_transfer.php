90f950e31d53b24171eca3827a82d26a25fd3e26
Andrej Ruman <aruman@nemetschek.com>
2015-02-16 15:50:38 +0100

    Fixed language issues Fixed checkout price display

--- catalog/language/slovak/payment/bank_transfer.php
+++ catalog/language/slovak/payment/bank_transfer.php
@@ -2,6 +2,6 @@
 // Text
 $_['text_title']       = 'Bankový prevod';
 $_['text_instruction'] = 'Pokyny pre bankový prevod';
-$_['text_description'] = 'Prosím převďte koncovú čiastku na nasledujúci bankový účet.';
+$_['text_description'] = 'Prosím preveďte koncovú čiastku na nasledujúci bankový účet.';
 $_['text_payment']     = 'Do doby prijatia Vašej platby vaša objednávka nebude expedovaná.';
 ?>
